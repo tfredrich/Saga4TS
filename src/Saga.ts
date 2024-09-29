2da2d7e96c6d069d9b0285af3fa74d2169764b68
Todd Fredrich <tfredrich@gmail.com>
2024-09-28 21:48:37 -0600

    Moved classes to /src/

--- src/Saga.ts
+++ src/Saga.ts
@@ -2,26 +2,34 @@ import { Observer } from './Observer';
 import { Step } from './Step';
 import { Context } from './Context';
 
-export class Saga {
+export class Saga
+{
     private steps: Step[];
     private observers: Observer[] = [];
 
-    constructor(steps: Step[]) {
+    constructor(steps: Step[])
+	{
         this.steps = steps;
     }
 
-    addObserver(observer: Observer): void {
+    addObserver(observer: Observer): void
+	{
         this.observers.push(observer);
     }
 
-    async run(context: Context): Promise<void> {
+    async run(context: Context): Promise<void>
+	{
 		var executedSteps: Step[] = [];
 
-        for (const step of this.steps) {
-            try {
+        for (const step of this.steps)
+		{
+            try
+			{
 				executedSteps.push(step);
                 await step.execute(context);
-            } catch (error) {
+            }
+			catch (error)
+			{
                 await this.compensateExecuted(context, executedSteps);
                 throw error;
             }
@@ -31,16 +39,23 @@ export class Saga {
         this.observers.forEach(observer => observer.onEvent('sagaCompleted'));
     }
 
-	public async compensate(context: Context): Promise<void> {
+	public async compensate(context: Context): Promise<void>
+	{
 		await this.compensateExecuted(context, this.steps);
 	}
 
-    private async compensateExecuted(context: Context, executedSteps: Step[]): Promise<void> {
+    private async compensateExecuted(context: Context, executedSteps: Step[]): Promise<void>
+	{
 		var steps = Array.from(executedSteps).reverse();
-        for (const step of steps) {
-            try {
+
+		for (const step of steps)
+		{
+            try
+			{
                 await step.compensate(context);
-            } catch (error) {
+            }
+			catch (error)
+			{
                 console.error(`Compensation failed for step:`, error);
             }
         }
