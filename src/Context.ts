8933cc67e679176af7573c58a7d4cc2fece8ce58
toddfredrich <toddfredrich@localhost>
2024-11-26 10:40:40 -0700

    Whitespace

--- src/Context.ts
+++ src/Context.ts
@@ -2,27 +2,25 @@ import { Observer } from './Observer';
 import { ContextEvent } from './ContextEvent';
 
 export class Context {
-    private observers: Observer<ContextEvent>[] = [];
-    private data: { [key: string]: any } = {};
+	private observers: Observer<ContextEvent>[] = [];
+	private data: { [key: string]: any } = {};
 
-    get(key: string): any {
-        return this.data[key];
-    }
+	get(key: string): any {
+		return this.data[key];
+	}
 
-    set(key: string, value: any): void {
-        if (this.data[key] !== value) {
-            this.data[key] = value;
-            this.notifyObservers(key, value);
-        }
-    }
+	set(key: string, value: any): void {
+		if (this.data[key] !== value) {
+			this.data[key] = value;
+			this.notifyObservers(key, value);
+		}
+	}
 
-    addObserver(observer: Observer<ContextEvent>): void
-	{
-        this.observers.push(observer);
-    }
+	addObserver(observer: Observer<ContextEvent>): void {
+		this.observers.push(observer);
+	}
 
-    private notifyObservers(key: string, value: any): void
-	{
-        this.observers.forEach(observer => observer.onEvent(new ContextEvent(key, value)));
-    }
+	private notifyObservers(key: string, value: any): void {
+		this.observers.forEach(observer => observer.onEvent(new ContextEvent(key, value)));
+	}
 }
